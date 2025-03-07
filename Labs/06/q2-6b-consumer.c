8558ac5051fe1f864123e4fb3fe22c4cd3d027ca
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-03-08 01:45:38 +0500

    Update q2-6b-consumer.c

--- Labs/06/q2-6b-consumer.c
+++ Labs/06/q2-6b-consumer.c
@@ -1,5 +1,3 @@
-// experient: 6b consumer
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,36 +5,33 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
-
 #define FIFO_FILE "/tmp/myfifo"
 
-int main() {
-	int fd;
-	char buffer[BUFSIZ];
-	ssize_t num_bytes;
+int main()
+{
+    int fd;
+    char buffer[BUFSIZ];
+    ssize_t num_bytes;
 
-	fd = open(FIFO_FILE, O_RDONLY);
-	if (fd == -1) {
-		perror("open");
-		exit(EXIT_FAILURE);
-	}
+    fd = open(FIFO_FILE, O_RDONLY);
+    if (fd == -1)
+    {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
 
-	while (1) {
-		num_bytes = read(fd, buffer, BUFSIZ);
-		if (num_bytes == -1) {
-			perror("read");
-			exit(EXIT_FAILURE);
-		} else if (num_bytes == 0) {
-			printf("exiting\n");
-			exit(EXIT_SUCCESS);
-		}
+    while (1)
+    {
+        num_bytes = read(fd, buffer, BUFSIZ);
+        if (num_bytes > 0)
+        {
+            buffer[num_bytes] = '\0';
+            printf("consumer: received message '%s'\n", buffer);
+            if (strcmp(buffer, "exit") == 0)
+                break;
+        }
+    }
 
-		buffer[num_bytes] = '\0';
-		printf("consumer: received message: %s\n", buffer);
-	}
-	
-	close(fd);
-
-	return 0;
+    close(fd);
+    return 0;
 }
-
